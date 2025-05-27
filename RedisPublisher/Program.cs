a10ff1c227fccbdb797d60774ae3f623ba9b4b91
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2025-05-27 18:33:20 +0300

    AddedMessageBroker

---
+++ RedisPublisher/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:1453");
+ISubscriber sub = redis.GetSubscriber();
+
+while (true)
+{
+    Console.WriteLine("Mesaj");
+    string message = Console.ReadLine();
+    await sub.PublishAsync("Kanal.*",message);    
+}
---
+++ RedisSubscriber/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:1453");
+ISubscriber sub = redis.GetSubscriber();
+
+
+    await sub.SubscribeAsync("Kanal.*" , (channel , value)=>{
+        Console.Write(value);
+    });
+
+    Console.Read();
